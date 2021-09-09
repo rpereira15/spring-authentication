8c40cfe9399ecfd10805399d81e3c21df873dca4
Rômulo Pereira <romulo.stads@hotmail.com>
2021-09-09 14:30:26 -0300

    Initial commit

---
+++ src/main/java/br/senac/spring/authentication/SpringAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package br.senac.spring.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthenticationApplication.class, args);
+	}
+
+}
